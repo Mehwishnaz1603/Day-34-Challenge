6c588ea5e73f905189e457e277253990298199e0
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-24 19:30:44 +0500

    day 34 challenge

---
+++ index.js
@@ -0,0 +1,12 @@
+//🚀 Day 34 Challenge: Start Coding! 🚀//
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+var squareRootOfNumber = Math.sqrt(144);
+console.log(squareRootOfNumber); // This line effectively calculates and shows the square root of 144.
+//Question 101: Generate a random integer between 1 and 10.
+function RandomInteger() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(RandomInteger()); // Outputs a random integer between 1 and 10
+//Question 102: Calculate and log the absolute difference between the number -5 and 5.
+var differenceOfNumbers = Math.abs(-5 - 5);
+console.log(differenceOfNumbers);
---
+++ index.ts
@@ -0,0 +1,20 @@
+               //🚀 Day 34 Challenge: Start Coding! 🚀//
+
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+
+let squareRootOfNumber: number = Math.sqrt(144);
+console.log(squareRootOfNumber); // This line effectively calculates and shows the square root of 144.
+
+//Question 101: Generate a random integer between 1 and 10.
+
+function RandomInteger(): number {
+    return Math.floor(Math.random() * 10 ) +1;
+}
+console.log(RandomInteger()); // Outputs a random integer between 1 and 10
+
+//Question 102: Calculate and log the absolute difference between the number -5 and 5.
+
+let differenceOfNumbers: number = Math.abs(-5 - 5);
+console.log(differenceOfNumbers); 
+
+
